27445985f3fab1350c7375db1a6004f7dafaeb8a
GitHub <957590979@qq.com>
2019-11-26 16:02:46 +0800

    意见反馈

--- src/main/java/org/crown/projects/mine/controller/FeedbackRestController.java
+++ src/main/java/org/crown/projects/mine/controller/FeedbackRestController.java
@@ -20,29 +20,23 @@
  */
 package org.crown.projects.mine.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.crown.common.annotations.Resources;
-import org.crown.common.utils.CustomerUtils;
 import org.crown.common.utils.JWTUtils;
 import org.crown.enums.AuthTypeEnum;
+import org.crown.framework.controller.SuperController;
 import org.crown.framework.responses.ApiResponses;
-import org.crown.projects.mine.model.dto.CustomerDTO;
-import org.crown.projects.mine.model.entity.Customer;
 import org.crown.projects.mine.model.entity.Feedback;
-import org.crown.projects.mine.model.entity.Member;
-import org.crown.projects.mine.model.parm.CustomerPARM;
 import org.crown.projects.mine.model.parm.FeedbackPARM;
 import org.crown.projects.mine.service.IFeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
-
-import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RestController;
-import org.crown.framework.controller.SuperController;
 
 /**
  * <p>
