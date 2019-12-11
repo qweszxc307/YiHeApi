26196adbe51073efdadd8b661b20f7b20118d2f3
GitHub <957590979@qq.com>
2019-12-11 17:06:38 +0800

    微信接口修改

--- src/main/java/org/crown/enums/OrderStatusEnum.java
+++ src/main/java/org/crown/enums/OrderStatusEnum.java
@@ -4,12 +4,17 @@ package org.crown.enums;
  * @author ykMa
  */
 public enum OrderStatusEnum {
+    /*订单支付状态*/
     INIT(1, "初始化，未付款"),
     PAY_UP(2, "已付款，未发货"),
     DELIVERED(3, "已发货，未收货"),
     RECEIVED(4,"已收货，未确认"),
     CONFIRMED(5, "已确认,未评价"),
-    RATED(6, "已评价，交易结束")
+    RATED(6, "已评价，交易结束"),
+    /*订单类型*/
+    COMMON_ORDER(0, "正常订单"),
+    RECOMMEND_ORDER(1, "推荐产品订单"),
+    RECOMMEND_SEND_ORDER(2, "推荐产品-赠礼订单")
     ;
 
     private Integer value;
