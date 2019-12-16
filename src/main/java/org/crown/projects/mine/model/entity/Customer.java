40baf80960d92fc5a90962f10cffe928f1cf1cfd
geralt <geraltgogo@163.com>
2019-12-16 17:39:27 +0800

    一元三张优惠券提交

--- src/main/java/org/crown/projects/mine/model/entity/Customer.java
+++ src/main/java/org/crown/projects/mine/model/entity/Customer.java
@@ -56,7 +56,7 @@ public class Customer extends BaseModel {
     @ApiModelProperty(notes = "union_id")
     private String unionId;
     @ApiModelProperty(notes = "上级id")
-    private String parentId;
+    private Integer parentId;
     @ApiModelProperty(notes = "会员号")
     private String memberNum;
     @ApiModelProperty(notes = "等级表id")
